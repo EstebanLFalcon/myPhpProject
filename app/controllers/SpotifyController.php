9ac4fa2d4fd24ca6dcb94a88bd98d43a65d704f1
EstebanLFalcon <estebanlf92@gmail.com>
2014-11-29 09:46:29 -0800

    no tronix

--- app/controllers/SpotifyController.php
+++ app/controllers/SpotifyController.php
@@ -2,19 +2,6 @@
 
 class SpotifyController extends BaseController {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
-
 	public function login()
 	{
 		$session = new SpotifyWebAPI\Session('78fe14946efe45f285d840b72bea40e4', '54db8e2ab9204bc08d09da67577c3bee', 'http://localhost:8000/');
@@ -24,7 +11,6 @@ class SpotifyController extends BaseController {
 		$s['authorizeUrl'] = $code;
 		Session::put('spotifySession', $session);
 		Session::put('api', $api);
-		//$s = '{ "name": "John" }' ;
 		echo json_encode($s);
 	}
 
